646760a70c5c3a98c14f6d3edbdb40d70eb79cc2
elena-kharchenko <elenakharchenko13@gmail.com>
2025-08-07 14:35:13 +0200

    Setup git

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,5 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Hello you, " + name + "!")
 }
-
